c7b17cf719ce9d5e277bd5b35e344acfaa141beb
Progressxon <robingood199945@gmail.com>
2024-11-14 17:43:27 +0300

    adding projects and README.txt

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Newtonsoft.Json;
+using Практика.Models;
+
+namespace MilitaryApp
+{
+    public partial class MainWindow : Window
+    {
+        public List<Soldier> Soldiers { get; set; }
+        public Soldier SelectedSoldier { get; set; }
+        public List<MilitaryUnit> MilitaryUnits { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadJsonData();
+        }
+
+        private void LoadJsonData()
+        {
+            string jsonFileName = @"C:\Users\Zver\Desktop\чет для учебы\лабы\Практика\var6.json";
+            string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, jsonFileName);
+
+            if (File.Exists(jsonFilePath))
+            {
+                try
+                {
+                    string jsonData = File.ReadAllText(jsonFilePath);
+                    var root = JsonConvert.DeserializeObject<Root>(jsonData);
+                    Soldiers = root.Военнослужащие;
+                    MilitaryUnits = root.ВоеннаяЧасть;
+                    DataContext = this;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка загрузки данных: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Файл {jsonFilePath} не найден", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void OnInfoButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (SelectedSoldier != null)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"ФИО: {SelectedSoldier.ФИО}");
+                sb.AppendLine($"Звание: {SelectedSoldier.Звание}");
+
+                if (SelectedSoldier.ВоеннаяЧасть >= 0 && SelectedSoldier.ВоеннаяЧасть < MilitaryUnits.Count)
+                {
+                    var militaryUnit = MilitaryUnits[SelectedSoldier.ВоеннаяЧасть];
+                    sb.AppendLine($"Военная часть: {militaryUnit.Название} ({militaryUnit.СубъектРФ})");
+                }
+
+                sb.AppendLine("Специализации:");
+                foreach (var spec in SelectedSoldier.Специализации)
+                {
+                    sb.AppendLine($"- {spec}");
+                }
+
+                MessageBox.Show(sb.ToString(), "Информация о военнослужащем");
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите военнослужащего", "Ошибка");
+            }
+        }
+
+        private void OnShowMilitaryUnitsButtonClick(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var unit in MilitaryUnits)
+            {
+                sb.AppendLine($"Название: {unit.Название}, Субъект РФ: {unit.СубъектРФ}");
+            }
+
+            MessageBox.Show(sb.ToString(), "Список военных частей");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x, y , шаг");
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double n = double.Parse(Console.ReadLine());
+            double[] a = new double[(int)(10 / n) + 1];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = fynk(x, y);
+                x += n;
+            }
+            vivod(a);
+            Console.WriteLine(a[0]);
+            Console.ReadKey();
+        }
+        public static double fynk(double x, double y)
+        {
+            if (x - y == 0)
+            {
+                return Math.Sin(Math.Pow(x, 2)) + Math.Cos(y);
+            }
+            else if (x - y > 0)
+            {
+                return Math.Tan(Math.Pow(x, 2) + y);
+            }
+            else
+            {
+                return Math.Pow(Math.Sin(x), 2) + Math.Cos(y * y);
+            }
+        }
+        public static void vivod(double[] a1)
+        {
+            for (int i = 0; i < a1.Length; i++)
+            {
+                Console.WriteLine("a[" + i + "]= " + a1[i]);
+            }
+
+        }
+        
+    }
+}
